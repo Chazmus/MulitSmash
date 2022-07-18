6336cd8c7abbb2c5bcab62035fc46141c01cdaf5
Chaz Bailey <chaz.bailey@forgerock.com>
2022-07-18 22:23:14 +0100

    Getting some movement into the player wip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,31 @@
+using System;
 using Photon.Pun;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviourPun
 {
-    public void OnMove()
+    private Rigidbody2D rigidBody;
+
+    [SerializeField]
+    private int speed;
+    private PlayerInput playerInput;
+
+    private void Awake()
     {
-        Debug.Log("poopy");
+        rigidBody = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        var val = value.Get<Vector2>();
+        rigidBody.AddForce(val * 10);
+        Debug.Log(val);
+    }
+
+    private void Update()
+    {
+        playerInput.actions
     }
 }
